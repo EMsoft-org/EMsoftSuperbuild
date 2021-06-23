d8717e0733367193c8c89891f99dcc9962500973
Michael Jackson <mike.jackson@bluequartz.net>
2021-06-23 09:12:51 -0400

    Properly update the generated EMsoft_SDK.cmake file with NLopt_DIR value
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- projects/nlopt.cmake
+++ projects/nlopt.cmake
@@ -7,15 +7,15 @@ if("${BUILD_NLOPT}" STREQUAL "OFF")
 endif()
 
 set(extProjectName "nlopt")
-set(nlopt_VERSION "2.7.0")
-message(STATUS "Building: ${extProjectName} ${nlopt_VERSION}: -DBUILD_NLOPT=${BUILD_NLOPT}" )
+set(NLopt_VERSION "2.7.0")
+message(STATUS "Building: ${extProjectName} ${NLopt_VERSION}: -DBUILD_NLOPT=${BUILD_NLOPT}" )
 
 if(MSVC_IDE)
-  set(nlopt_INSTALL "${EMsoft_SDK}/${extProjectName}-${nlopt_VERSION}")
+  set(nlopt_INSTALL "${EMsoft_SDK}/${extProjectName}-${NLopt_VERSION}")
 elseif(WIN32)
-  set(nlopt_INSTALL "${EMsoft_SDK}/${extProjectName}-${nlopt_VERSION}-${CMAKE_BUILD_TYPE}")
+  set(nlopt_INSTALL "${EMsoft_SDK}/${extProjectName}-${NLopt_VERSION}-${CMAKE_BUILD_TYPE}")
 else()
-  set(nlopt_INSTALL "${EMsoft_SDK}/${extProjectName}-${nlopt_VERSION}-${CMAKE_BUILD_TYPE}")
+  set(nlopt_INSTALL "${EMsoft_SDK}/${extProjectName}-${NLopt_VERSION}-${CMAKE_BUILD_TYPE}")
 endif()
 
 set_property(DIRECTORY PROPERTY EP_BASE ${EMsoft_SDK}/superbuild)
@@ -23,25 +23,25 @@ set_property(DIRECTORY PROPERTY EP_BASE ${EMsoft_SDK}/superbuild)
 
 # if(DREAM3D_USE_CUSTOM_DOWNLOAD_SITE)
 #   set(EP_SOURCE_ARGS  
-#     DOWNLOAD_NAME ${extProjectName}-${nlopt_VERSION}.zip
-#     URL ${DREAM3D_CUSTOM_DOWNLOAD_URL_PREFIX}${extProjectName}-${nlopt_VERSION}.zip
+#     DOWNLOAD_NAME ${extProjectName}-${NLopt_VERSION}.zip
+#     URL ${DREAM3D_CUSTOM_DOWNLOAD_URL_PREFIX}${extProjectName}-${NLopt_VERSION}.zip
 #   )
 # else()
   set(EP_SOURCE_ARGS  
     GIT_REPOSITORY "https://github.com/stevengj/nlopt.git"
     GIT_PROGRESS 1
-    GIT_TAG v${nlopt_VERSION}
+    GIT_TAG v${NLopt_VERSION}
   )
 #endif()
 
 ExternalProject_Add(${extProjectName}
   ${EP_SOURCE_ARGS}
 
-  TMP_DIR "${EMsoft_SDK}/superbuild/${extProjectName}-${nlopt_VERSION}/tmp/${CMAKE_BUILD_TYPE}"
-  STAMP_DIR "${EMsoft_SDK}/superbuild/${extProjectName}-${nlopt_VERSION}/Stamp"
-  DOWNLOAD_DIR ${EMsoft_SDK}/superbuild/${extProjectName}-${nlopt_VERSION}/Download
-  SOURCE_DIR "${EMsoft_SDK}/superbuild/${extProjectName}-${nlopt_VERSION}/Source"
-  BINARY_DIR "${EMsoft_SDK}/superbuild/${extProjectName}-${nlopt_VERSION}/Build/${CMAKE_BUILD_TYPE}"
+  TMP_DIR "${EMsoft_SDK}/superbuild/${extProjectName}-${NLopt_VERSION}/tmp/${CMAKE_BUILD_TYPE}"
+  STAMP_DIR "${EMsoft_SDK}/superbuild/${extProjectName}-${NLopt_VERSION}/Stamp"
+  DOWNLOAD_DIR ${EMsoft_SDK}/superbuild/${extProjectName}-${NLopt_VERSION}/Download
+  SOURCE_DIR "${EMsoft_SDK}/superbuild/${extProjectName}-${NLopt_VERSION}/Source"
+  BINARY_DIR "${EMsoft_SDK}/superbuild/${extProjectName}-${NLopt_VERSION}/Build/${CMAKE_BUILD_TYPE}"
   INSTALL_DIR "${nlopt_INSTALL}"
 
   CMAKE_ARGS
@@ -76,11 +76,11 @@ FILE(APPEND ${EMsoft_SDK_FILE} "\n")
 FILE(APPEND ${EMsoft_SDK_FILE} "#--------------------------------------------------------------------------------------------------\n")
 FILE(APPEND ${EMsoft_SDK_FILE} "# haru\n")
 if(APPLE)
-  FILE(APPEND ${EMsoft_SDK_FILE} "set(nlopt_DIR \"\${EMsoft_SDK_ROOT}/${extProjectName}-${nlopt_VERSION}-\${BUILD_TYPE}/cmake/lib${extProjectName}\" CACHE PATH \"\")\n")
+  FILE(APPEND ${EMsoft_SDK_FILE} "set(NLopt_DIR \"\${EMsoft_SDK_ROOT}/${extProjectName}-${NLopt_VERSION}-\${BUILD_TYPE}/lib/cmake/${extProjectName}\" CACHE PATH \"\")\n")
 elseif(WIN32)
-  FILE(APPEND ${EMsoft_SDK_FILE} "set(nlopt_DIR \"\${EMsoft_SDK_ROOT}/${extProjectName}-${nlopt_VERSION}/cmake/lib${extProjectName}\" CACHE PATH \"\")\n")
+  FILE(APPEND ${EMsoft_SDK_FILE} "set(NLopt_DIR \"\${EMsoft_SDK_ROOT}/${extProjectName}-${NLopt_VERSION}/lib/cmake/${extProjectName}\" CACHE PATH \"\")\n")
 else()
-  FILE(APPEND ${EMsoft_SDK_FILE} "set(nlopt_DIR \"\${EMsoft_SDK_ROOT}/${extProjectName}-${nlopt_VERSION}-\${BUILD_TYPE}/cmake/lib${extProjectName}\" CACHE PATH \"\")\n")
+  FILE(APPEND ${EMsoft_SDK_FILE} "set(NLopt_DIR \"\${EMsoft_SDK_ROOT}/${extProjectName}-${NLopt_VERSION}-\${BUILD_TYPE}/lib/cmake/lib${extProjectName}\" CACHE PATH \"\")\n")
 endif()
-FILE(APPEND ${EMsoft_SDK_FILE} "set(CMAKE_MODULE_PATH \${CMAKE_MODULE_PATH} \${nlopt_DIR})\n")
-FILE(APPEND ${EMsoft_SDK_FILE} "set(nlopt_VERSION \"${nlopt_VERSION}\" CACHE STRING \"\")\n")
+FILE(APPEND ${EMsoft_SDK_FILE} "set(CMAKE_MODULE_PATH \${CMAKE_MODULE_PATH} \${NLopt_DIR})\n")
+FILE(APPEND ${EMsoft_SDK_FILE} "set(NLopt_VERSION \"${NLopt_VERSION}\" CACHE STRING \"\")\n")
